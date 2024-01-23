e578428dc2044acba244cdf05701b4b31320c2b5
jitendravaid8 <jitendravaid8@gmail.com>
2024-01-23 19:21:07 +0100

    updating app.yaml file

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const  { Pool } = require('pg')
 
 const pool = new Pool({
   user: 'postgres',
-  host: '34.141.149.241',
+  host: '/cloudsql/ingka-pdpp-absence-dev:europe-west4:ppdh-absence-dev-db',
   database: 'absenceDB',
   password: 'postgres1234',
   port: 5432,
