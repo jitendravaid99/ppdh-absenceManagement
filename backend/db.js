049332082019dbe333b8f9e5cd5a0cf617e7fc0b
jitendravaid8 <jitendravaid8@gmail.com>
2024-01-23 19:31:31 +0100

    updating db and yaml  file

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const  { Pool } = require('pg')
 
 const pool = new Pool({
-  user: 'postgres',
-  host: '/cloudsql/ingka-pdpp-absence-dev:europe-west4:ppdh-absence-dev-db',
-  database: 'absenceDB',
-  password: 'postgres1234',
-  port: 5432,
+  user: process.env.DB_USER || 'postgres',
+  host: process.env.DB_HOST || '/cloudsql/ingka-pdpp-absence-dev:europe-west4:ppdh-absence-dev-db',
+  database: process.env.DB_NAME || 'absenceDB',
+  password: process.env.DB_PASSWORD || 'postgres1234',
+  port: process.env.DB_PORT || 5432,
 });
 
 module.exports = pool;
